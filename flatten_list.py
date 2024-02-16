fa01187484e14659e9f82742a05db8da37e43f8a
Jose Sandoval <Software.Sandoval@gmail.com>
2024-02-16 15:25:32 -0600

    init

--- flatten_list.py
+++ flatten_list.py
@@ -1,2 +1,22 @@
-def flatten_list(lst):
-    pass
+def flatten_list(lst, i=0, result = []):
+    result = []
+
+    for item in lst:
+        if isinstance(item, int):
+            result.append(item)
+        elif isinstance(item,list):
+            # print(result)
+            result.extend(flatten_list(item))
+    return result
+
+
+tst1 = [1, 2, [3], [4,5]]
+# output = [1, 2, 3, 4, 5]
+tst2 = [1, [2], [3, [4, 5, [6]]], 7]
+# output = [1, 2, 3, 4, 5, 6, 7]
+
+print(flatten_list(tst1))
+print(flatten_list(tst2))
+
+
+
